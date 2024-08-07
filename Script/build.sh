f0769fe40332695e9c3ce6265ae949daa69141d9
Anbalagan D <anbu94p@gmail.com>
2024-08-07 22:21:04 +0530

    Use dummy script for testing purpose

--- Script/build.sh
+++ Script/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-xcodebuild -project MusicPlayer.xcodeproj -target MusicPlayer -configuration Release
+# xcodebuild -project MusicPlayer.xcodeproj -target MusicPlayer -configuration Release
+
+echo "Dummy build file" > build.txt
