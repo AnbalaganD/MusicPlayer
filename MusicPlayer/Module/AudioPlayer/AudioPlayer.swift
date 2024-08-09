58016be6ab7fbfb2d9a8a4e2340c133415febe34
Anbalagan D <anbu94p@gmail.com>
2024-08-09 17:39:17 +0530

    Add time observer to AVplayer

--- MusicPlayer/Module/AudioPlayer/AudioPlayer.swift
+++ MusicPlayer/Module/AudioPlayer/AudioPlayer.swift
@@ -13,14 +13,47 @@ final class AudioPlayerView: UIView {
 
     private(set) var isPlaying: Bool?
     private(set) var status: PlayerStatus = .unknown
+    private var boundaryTimeObserverToken: Any?
 
     init(player: AVPlayer = AVPlayer()) {
         self.player = player
         super.init(frame: .zero)
+        addPeriodicTimeObserver()
+        setAds()
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    private func addPeriodicTimeObserver() {
+        player.addPeriodicTimeObserver(
+            forInterval: CMTime(seconds: 1, preferredTimescale: 1),
+            queue: nil
+        ) { time in
+            print(time.seconds)
+            print(Thread.current)
+        }
+    }
+
+    private func setAds() {
+        boundaryTimeObserverToken = player.addBoundaryTimeObserver(
+            forTimes: [
+                NSValue(time: CMTime(seconds: 10, preferredTimescale: 1)),
+                NSValue(time: CMTime(seconds: 20, preferredTimescale: 1)),
+                NSValue(time: CMTime(seconds: 30, preferredTimescale: 1)),
+                NSValue(time: CMTime(seconds: 40, preferredTimescale: 1)),
+                NSValue(time: CMTime(seconds: 50, preferredTimescale: 1)),
+                NSValue(time: CMTime(seconds: 60, preferredTimescale: 1))
+            ],
+            queue: nil) {[weak self] in
+                print("Show ads here")
+                guard let self else { return }
+                player.pause()
+                DispatchQueue.main.asyncAfter(deadline: .now() + 10) {
+                    self.player.play()
+                }
+            }
     }
 }
 
